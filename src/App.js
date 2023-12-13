aa9e50abc052a68079c70a3fd5f37b55b298a620
SalmaHatim <hatimsalma02@gmail.com>
2023-12-13 21:35:08 +0100

    react-tp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Counter from './components/Counter.js'; 
-/*6- Testing: Render the Counter component in the main App component */
+
 const App = () => {
   return (
     <div>
